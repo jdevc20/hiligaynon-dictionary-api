5f25b41c3e6ccaff7ceb113120a2ff9599fa2009
jdevc920 <jdcamarin20@gmail.com>
2024-05-11 00:08:00 +0800

    [_]

--- mongo.js
+++ mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Connection URI
-const mongoURI = 'mongodb://localhost:27017/ilonggogid';
+const mongoURI = 'mongodb+srv://johndavecamarin20:Bacolod123!@cluster0.gqouffi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 // Function to connect to MongoDB
 const connectDB = async () => {
